baaf66268690756335f00093014d51f309113f14
Imad Eddine Kettaf <imedkettaf@gmail.com>
2023-09-29 13:31:48 +0100

    Completed Assignment

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    let words = tutorial.split(" ");
+    tutorial = words.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    return tutorial.join(' ');
+  })
 }
+
+console.log(tutorials)
